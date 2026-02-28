4f8d205f99bb8ef6e58352735d6d6f96c74b1d40
sumankumar36653 <sumankumar36653@gmail.com>
2026-02-28 14:49:07 +0530

    Add frontend app files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Simple Task Management App</h1>
+    </main>
+  );
+}
